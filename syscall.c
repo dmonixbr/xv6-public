a02e7f1fb943ff7ae20f351606e5da1fdca1cf4e
Victor Vieira de Melo <victorvmelo@gmail.com>
2022-10-18 17:11:42 -0300

    Função trace funcionando

--- syscall.c
+++ syscall.c
@@ -130,6 +130,31 @@ static int (*syscalls[])(void) = {
 [SYS_trace]   sys_trace,
 };
 
+static char* (syscallsNames[]) = {
+[SYS_fork]    "sys_fork",
+[SYS_exit]    "sys_exit",
+[SYS_wait]    "sys_wait",
+[SYS_pipe]    "sys_pipe",
+[SYS_read]    "sys_read",
+[SYS_kill]    "sys_kill",
+[SYS_exec]    "sys_exec",
+[SYS_fstat]   "sys_fstat",
+[SYS_chdir]   "sys_chdir",
+[SYS_dup]     "sys_dup",
+[SYS_getpid]  "sys_getpid",
+[SYS_sbrk]    "sys_sbrk",
+[SYS_sleep]   "sys_sleep",
+[SYS_uptime]  "sys_uptime",
+[SYS_open]    "sys_open",
+[SYS_write]   "sys_write",
+[SYS_mknod]   "sys_mknod",
+[SYS_unlink]  "sys_unlink",
+[SYS_link]    "sys_link",
+[SYS_mkdir]   "sys_mkdir",
+[SYS_close]   "sys_close",
+[SYS_trace]   "sys_trace",
+};
+
 void
 syscall(void)
 {
@@ -141,7 +166,7 @@ syscall(void)
     curproc->tf->eax = syscalls[num]();
     curproc->numOfSysCalls++;
     if(curproc->traced){
-      cprintf("ID: %d NOME: %s NUM_CALL: %d\n", curproc->pid, curproc->name, num);
+      cprintf("ID: %d NOME: %s NUM_CALL: %d CALL_NAME: %s\n", curproc->pid, curproc->name, num, syscallsNames[num]);
     }
   } else {
     cprintf("%d %s: unknown sys call %d\n",
